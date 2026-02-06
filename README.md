e153b668e88e3132d676f76212f33d970b665116
LiamBirrell <liam.birrell31@gmail.com>
2026-02-06 15:24:52 +1000

    Created initial Callback function for Benders'. finds initial solution but is slow, requires stronger cuts

--- README.md
+++ README.md
@@ -5,12 +5,6 @@
 A discrete time traffic optimisation framework utilising Logic-Based Benders Decomposition. Integrates a Gurobi Master Problem for traffic light schedule generation with a custom simulation Subproblem, using iterative callbacks to converge on optimal traffic light schedule.
 
 TO-DO:
-* # Gurobi - Final Boss
-* ~~Master Problem - Traffic Light Sequence generation~~
-    * Fix constraints so they obey time limit laws - its breaking for some reason
-* ~~Sub Problem - Simulation~~
-* Apply Benders' Decomposition algorithm (Callback)
-    * Run BMP solution through BSP (simulation)
-    * Check if "cars exited" is a good value
-    * if not - tell gurobi to not use that pattern again
-    * keep going until the roof has been lowered enough that an optimal solution has been found that passes as many cars through the simulation as possible 
+* Gurobi - Final Boss
+    * Add lazyconstraint that increasese the percentage of time each lane can be green based on how congested they were in the simulation
+    * maybe more, not sure yet. 
--- Traffic_Optimisation.py
+++ Traffic_Optimisation.py
@@ -91,4 +91,32 @@ for (i,j) in optimised_light_schedule:
 for (i,j) in optimised_light_schedule:
     BMP.addConstr(gp.quicksum(X[i,j,t] for t in range(sim_length)) >= 0.20 * sim_length)
     
+def Callback(model, where):
+    if where == gp.GRB.Callback.MIPSOL:
+        # Get Gurobi's proposed schedule
+        XV = model.cbGetSolution(X)
+        
+        # Reconstruct Schedule & Run Simulation
+        current_schedule = {}
+        for (i, j) in optimised_light_schedule:
+            current_schedule[(i, j)] = [int(round(XV[i, j, t])) for t in range(sim_length)]
+            
+        _i, _v, _r, _l, current_score = simulation(current_schedule, vehicles, routes, neighbour_map)
+        
+        # Identify Green Lights
+        current_on_keys = [k for k in X if XV[k] > 0.5]
+        
+        # Build the Hamming Distance Expression
+        # Distance = (N_On) + (Sum_All_Vars) - 2*(Sum_Active_Vars)
+        val_n_on = len(current_on_keys)
+        expr_sum_all = gp.quicksum(X.values())
+        expr_sum_active = gp.quicksum(X[k] for k in current_on_keys)
+        
+        dist_expr = val_n_on + expr_sum_all - 2*expr_sum_active
+        
+        # Add the cut
+        model.cbLazy(Theta <= current_score + num_vehicles * dist_expr)               
+                
+BMP.setParam("LazyConstraints", 1)
+BMP.optimize(Callback)
 BMP.optimize()
